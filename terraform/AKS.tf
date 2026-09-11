43168483cdefec30f649dabb00f2cb9b4f0bc923
Iaemrys101 <Nwachukwuemmanuel938@gmail.com>
2026-09-11 02:49:06 +0100

    Update website and fix AKS OIDC deployment configuration

--- terraform/AKS.tf
+++ terraform/AKS.tf
@@ -4,10 +4,19 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = local.aks_name
 
+  azure_active_directory_role_based_access_control {
+    tenant_id          = data.azurerm_client_config.current.tenant_id
+    azure_rbac_enabled = true
+  }
+
   default_node_pool {
     name       = "default"
     node_count = var.aks_node_count
     vm_size    = var.aks_vm_size
+
+    upgrade_settings {
+      max_surge = "10%"
+    }
   }
 
   node_provisioning_profile {
--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -10,3 +10,18 @@ resource "azurerm_role_assignment" "you_kv_secrets_officer" {
   role_definition_name = "Key Vault Secrets Officer"
   principal_id         = data.azurerm_client_config.current.object_id
 }
+
+# Preserve the existing GitHub deployment identity's AKS access.
+resource "azurerm_role_assignment" "github_aks_user" {
+  scope                = azurerm_kubernetes_cluster.aks.id
+  role_definition_name = "Azure Kubernetes Service Cluster User Role"
+  principal_id         = "3cc39f8b-ae8c-4720-86ad-f4c84ea375ee"
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "github_aks_writer" {
+  scope                = "${azurerm_kubernetes_cluster.aks.id}/namespaces/default"
+  role_definition_name = "Azure Kubernetes Service RBAC Writer"
+  principal_id         = "3cc39f8b-ae8c-4720-86ad-f4c84ea375ee"
+  principal_type       = "ServicePrincipal"
+}
